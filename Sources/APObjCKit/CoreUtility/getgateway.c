877b48e96f070d575577106a6362400247aac367
prince_geo <prince.geovision.tw@gmail.com>
2022-06-23 18:29:07 +0800

    add c code

---
+++ Sources/APObjCKit/CoreUtility/getgateway.c
@@ -0,0 +1,254 @@
+/* $Id: getgateway.c,v 1.6 2007/12/13 14:46:06 nanard Exp $ */
+/* libnatpmp
+ * Copyright (c) 2007, Thomas BERNARD <miniupnp@free.fr>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. */
+#include <stdio.h>
+#include <ctype.h>
+#include <netinet/in.h>
+#include <sys/param.h>
+#include <string.h>
+#if defined(BSD) || defined(__APPLE__)
+#include <stdlib.h>
+#include <sys/sysctl.h>
+#include <sys/socket.h>
+
+#if TARGET_IPHONE_SIMULATOR
+#include <net/route.h>
+#else
+#include "route.h"
+#endif
+
+//#if TARGET_OS_IPHONE
+//#else
+//#include <net/route.h>
+//#endif
+
+#include <net/if.h>
+#endif
+#include "getgateway.h"
+
+#ifdef __linux__
+int getdefaultgateway(in_addr_t * addr)
+{
+    long d, g;
+    char buf[256];
+    int line = 0;
+    FILE * f;
+    char * p;
+    f = fopen("/proc/net/route", "r");
+    if(!f)
+        return -1;
+    while(fgets(buf, sizeof(buf), f)) {
+        if(line > 0) {
+            p = buf;
+            while(*p && !isspace(*p))
+                p++;
+            while(*p && isspace(*p))
+                p++;
+            if(sscanf(p, "%lx%lx", &d, &g)==2) {
+                if(d == 0) { /* default */
+                    *addr = g;
+                    fclose(f);
+                    return 0;
+                }
+            }
+        }
+        line++;
+    }
+    /* not found ! */
+    if(f)
+        fclose(f);
+    return -1;
+}
+#endif
+
+#if defined(BSD) || defined(__APPLE__)
+
+#define ROUNDUP(a) \
+((a) > 0 ? (1 + (((a) - 1) | (sizeof(long) - 1))) : sizeof(long))
+
+int getdefaultgateway(in_addr_t * addr)
+{
+#if 0
+    /* net.route.0.inet.dump.0.0 ? */
+    int mib[] = {CTL_NET, PF_ROUTE, 0, AF_INET,
+        NET_RT_DUMP, 0, 0/*tableid*/};
+#endif
+    /* net.route.0.inet.flags.gateway */
+    int mib[] = {CTL_NET, PF_ROUTE, 0, AF_INET,
+        NET_RT_FLAGS, RTF_GATEWAY};
+    size_t l;
+    char * buf, * p;
+    struct rt_msghdr * rt;
+    struct sockaddr * sa;
+    struct sockaddr * sa_tab[RTAX_MAX];
+    int i;
+    int r = -1;
+    if(sysctl(mib, sizeof(mib)/sizeof(int), 0, &l, 0, 0) < 0) {
+        return -1;
+    }
+    if(l>0) {
+        buf = malloc(l);
+        if(sysctl(mib, sizeof(mib)/sizeof(int), buf, &l, 0, 0) < 0) {
+            return -1;
+        }
+        for(p=buf; p<buf+l; p+=rt->rtm_msglen) {
+            rt = (struct rt_msghdr *)p;
+            sa = (struct sockaddr *)(rt + 1);
+            for(i=0; i<RTAX_MAX; i++) {
+                if(rt->rtm_addrs & (1 << i)) {
+                    sa_tab[i] = sa;
+                    sa = (struct sockaddr *)((char *)sa + ROUNDUP(sa->sa_len));
+                } else {
+                    sa_tab[i] = NULL;
+                }
+            }
+            if( ((rt->rtm_addrs & (RTA_DST|RTA_GATEWAY)) == (RTA_DST|RTA_GATEWAY))
+               && sa_tab[RTAX_DST]->sa_family == AF_INET
+               && sa_tab[RTAX_GATEWAY]->sa_family == AF_INET) {
+                if(((struct sockaddr_in *)sa_tab[RTAX_DST])->sin_addr.s_addr == 0) {
+                    *addr = ((struct sockaddr_in *)(sa_tab[RTAX_GATEWAY]))->sin_addr.s_addr;
+                    r = 0;
+                }
+            }
+        }
+        free(buf);
+    }
+    return r;
+}
+
+
+// from http://stackoverflow.com/questions/2300149/how-can-i-determine-the-default-gateway-on-iphone
+int getdefaultgatewaybyname(const char* name, in_addr_t * addr)
+{
+#if 0
+    /* net.route.0.inet.dump.0.0 ? */
+    int mib[] = {CTL_NET, PF_ROUTE, 0, AF_INET,
+        NET_RT_DUMP, 0, 0/*tableid*/};
+#endif
+    /* net.route.0.inet.flags.gateway */
+    int mib[] = {CTL_NET, PF_ROUTE, 0, AF_INET,
+        NET_RT_FLAGS, RTF_GATEWAY};
+    size_t l;
+    char * buf, * p;
+    struct rt_msghdr * rt;
+    struct sockaddr * sa;
+    struct sockaddr * sa_tab[RTAX_MAX];
+    int i;
+    int r = -1;
+    if(sysctl(mib, sizeof(mib)/sizeof(int), 0, &l, 0, 0) < 0) {
+        return -1;
+    }
+    if(l>0) {
+        buf = malloc(l);
+        if(sysctl(mib, sizeof(mib)/sizeof(int), buf, &l, 0, 0) < 0) {
+            return -1;
+        }
+        for(p=buf; p<buf+l; p+=rt->rtm_msglen) {
+            rt = (struct rt_msghdr *)p;
+            sa = (struct sockaddr *)(rt + 1);
+            for(i=0; i<RTAX_MAX; i++) {
+                if(rt->rtm_addrs & (1 << i)) {
+                    sa_tab[i] = sa;
+                    sa = (struct sockaddr *)((char *)sa + ROUNDUP(sa->sa_len));
+                } else {
+                    sa_tab[i] = NULL;
+                }
+            }
+            if( ((rt->rtm_addrs & (RTA_DST|RTA_GATEWAY)) == (RTA_DST|RTA_GATEWAY))
+               && sa_tab[RTAX_DST]->sa_family == AF_INET
+               && sa_tab[RTAX_GATEWAY]->sa_family == AF_INET) {
+                if(((struct sockaddr_in *)sa_tab[RTAX_DST])->sin_addr.s_addr == 0) {
+                    char ifName[128];
+                    if_indextoname(rt->rtm_index,ifName);
+                    if(strcmp(name,ifName)==0){
+                        *addr = ((struct sockaddr_in *)(sa_tab[RTAX_GATEWAY]))->sin_addr.s_addr;
+                        r = 0;
+                    }
+                }
+            }
+        }
+        free(buf);
+    }
+    return r;
+}
+
+int getdefaultgateway_ios(in_addr_t * addr, unsigned char out_gateway[])
+{
+#if 0
+    /* net.route.0.inet.dump.0.0 ? */
+    int mib[] = {CTL_NET, PF_ROUTE, 0, AF_INET,
+        NET_RT_DUMP, 0, 0/*tableid*/};
+#endif
+    /* net.route.0.inet.flags.gateway */
+    int mib[] = {CTL_NET, PF_ROUTE, 0, AF_INET,
+        NET_RT_FLAGS, RTF_GATEWAY};
+    size_t l;
+    char * buf, * p;
+    struct rt_msghdr * rt;
+    struct sockaddr * sa;
+    struct sockaddr * sa_tab[RTAX_MAX];
+    int i;
+    int r = -1;
+    
+    
+    unsigned char octet[4]  = {0,0,0,0};
+    
+    if(sysctl(mib, sizeof(mib)/sizeof(int), 0, &l, 0, 0) < 0) {
+        return -1;
+    }
+    if(l>0) {
+        buf = malloc(l);
+        if(sysctl(mib, sizeof(mib)/sizeof(int), buf, &l, 0, 0) < 0) {
+            return -1;
+        }
+        for(p=buf; p<buf+l; p+=rt->rtm_msglen) {
+            rt = (struct rt_msghdr *)p;
+            sa = (struct sockaddr *)(rt + 1);
+            for(i=0; i<RTAX_MAX; i++) {
+                if(rt->rtm_addrs & (1 << i)) {
+                    sa_tab[i] = sa;
+                    sa = (struct sockaddr *)((char *)sa + ROUNDUP(sa->sa_len));
+                } else {
+                    sa_tab[i] = NULL;
+                }
+            }
+            
+            if( ((rt->rtm_addrs & (RTA_DST|RTA_GATEWAY)) == (RTA_DST|RTA_GATEWAY))
+               && sa_tab[RTAX_DST]->sa_family == AF_INET
+               && sa_tab[RTAX_GATEWAY]->sa_family == AF_INET) {
+                
+                
+                for (int i=0; i<4; i++){
+                    out_gateway[i] = ( ((struct sockaddr_in *)(sa_tab[RTAX_GATEWAY]))->sin_addr.s_addr >> (i*8) ) & 0xFF;
+                    octet[i] = ( ((struct sockaddr_in *)(sa_tab[RTAX_GATEWAY]))->sin_addr.s_addr >> (i*8) ) & 0xFF;
+                }
+                
+                if(((struct sockaddr_in *)sa_tab[RTAX_DST])->sin_addr.s_addr == 0) {
+                    if(octet[0] == 192 && octet[1] == 168){
+                        *addr = ((struct sockaddr_in *)(sa_tab[RTAX_GATEWAY]))->sin_addr.s_addr;
+                        r = 0;
+                        //printf("gateway address--%d.%d.%d.%d\n",octet[0],octet[1],octet[2],octet[3]);
+                        //printf("[2]gateway address--%d.%d.%d.%d\n",out_gateway[0],out_gateway[1],out_gateway[2],out_gateway[3]);
+                    }
+                }
+            }
+        }
+        //free(buf);
+    }
+    return r;
+
+}
+
+#endif // #if defined(BSD) || defined(__APPLE__)
